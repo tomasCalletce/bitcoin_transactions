f47cebdd24c0e36e61b1eaaff40d30ef6ca76b3f
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2022-06-22 16:38:26 -0500

    initial setup

--- src/State.java
+++ src/State.java
@@ -3,18 +3,31 @@ import java.util.ArrayList;
 
 public class State {
 
-    private static HashMap<Account,ArrayList<UTXO>> UTXOperAccount = new HashMap<Account,ArrayList<UTXO>>();
     private static HashMap<UTXO,Boolean> UTXOinSystem = new HashMap<UTXO,Boolean>();
 
     public static void StateTransition(Transaction _transaction) throws Exception {
         if(!UTXOinSystem.get(_transaction)){
             throw new Error("UTXO is not in S");
         }
-        Account _from = _transaction.getFrom();
-        if(!_transaction.getFrom().verifySignature(_transaction.getId(),_transaction.getSignature())){
+        if(!_transaction.getFrom().verifySignature(_transaction.getData(),_transaction.getSignature())){
             throw new Error("from not signer");
         }
+        if(_transaction.getInputBalanceWei() < _transaction.getOutputBalanceWei()){
+            throw new Error("input value < output value");
+        }
 
+    }
+
+    private static void RemovefromS(UTXO[] _transactions){
+        for (UTXO utxo : _transactions) {
+            State.UTXOinSystem.remove(utxo);
+        }
+    }
+
+    private static void AddtoS(UTXO[] _transactions){
+        for (UTXO utxo : _transactions) {
+            State.UTXOinSystem.put(utxo,true);
+        }
     }
     
     
--- src/Transaction.java
+++ src/Transaction.java
@@ -2,66 +2,59 @@ import java.util.Random;
 
 public class Transaction {
 
-    private UTXO inputs[];
+    private UTXO[] inputs;
+    private UTXO[] outputs;
     private Account from;
     private Account to;
-    private int outputWei;
     private int inputBalanceWei;
+    private int outputBalanceWei;
     private int tipWei;
-    private byte[] id;
-    private byte[] signature;
     private byte[] data;
+    private byte[] signature;
 
 
-    public Transaction(UTXO _inputs[],int _outputWei,Account _from,byte[] _signature, Account _to) throws Exception{
+    public Transaction(UTXO[] _inputs,UTXO[] _outputs,Account _from, Account _to){
         this.from = _from;
         this.to = _to;
         this.inputs = _inputs;
-        this.outputWei = _outputWei;
-        calculateInputBalance();
-        calculateTip();
-        if(this.inputBalanceWei < this.outputWei){
-            throw new Error("Transaction Input < Transaction Output");
-        }
-        this.id = Transaction.getRandomNumberInts().getBytes();
-        this.signature = _signature;
+        this.outputs = _outputs;
+        this.inputBalanceWei = Transaction.calculateSum(_inputs);
+        this.outputBalanceWei = Transaction.calculateSum(_outputs);
+        this.tipWei = this.inputBalanceWei - this.outputBalanceWei;
+        this.data = this.makeData().getBytes();
     }
 
-    private void calculateInputBalance(){
-        int _inputBalance = 0;
-        for (UTXO utxo : inputs) {
-            _inputBalance += utxo.getvalueInWei();
+    private static int calculateSum(UTXO[] _transactions){
+        int _sum = 0;
+        for (UTXO utxo : _transactions) {
+            _sum += utxo.getvalueInWei();
         }
-        this.inputBalanceWei = _inputBalance;
-    }
-
-    private void calculateTip(){
-        this.tipWei = this.inputBalanceWei - this.outputWei;
+        return _sum;
     }
     
-    private static String getRandomNumberInts(){
-        int _min = 1000000;
-        int _max = 1000000000;
-        Random _random = new Random();
-        return String.valueOf(_random.ints(_min,(_max+1)).findFirst().getAsInt());
-        
-    }
-
     private String makeData(){
-        return "{ " + this.from.getKeyPair().getPublic() + ":" + this.inputBalanceWei + " ," + this.from.getKeyPair().getPublic() + ":" + this.outputWei + " }" + " tip: " + this.tipWei + " .";
+        return "{ " + this.from.getKeyPair().getPublic() + ":" + this.inputBalanceWei + " ," + this.from.getKeyPair().getPublic() + ":" + this.outputBalanceWei + " }" + " tip:" + this.tipWei ;
     }
 
     public Account getFrom(){
         return this.from;
     }
 
-    public byte[] getId(){
-        return this.id;
-    }
-
     public byte[] getSignature(){
         return this.signature;
     }
 
-    
+    public byte[] getData(){
+        return this.data;
+    }
+
+    public int getInputBalanceWei(){
+       return this.inputBalanceWei;
+    }
+
+    public int getOutputBalanceWei(){
+        return this.outputBalanceWei;
+     }
+
+
 }
