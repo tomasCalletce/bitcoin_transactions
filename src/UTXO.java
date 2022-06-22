7190313a037fe262caafbe8a20c419739ad69830
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2022-06-21 22:04:28 -0500

    setup

---
+++ src/UTXO.java
@@ -0,0 +1,11 @@
+import java.util.HashMap; 
+import java.util.ArrayList;
+
+public class UTXO {
+
+    //private static HashMap<UTXO,Account> Owner = new HashMap<UTXO,Account>();
+    //private static HashMap<Account,ArrayList<UTXO>> AllUTXOofAccount = new HashMap<Account,ArrayList<UTXO>>();
+
+    
+
+}
